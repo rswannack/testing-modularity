77b8c5a531de1ca7e5f146ccbbe36c558f8cd881
rswannack <32341198+rswannack@users.noreply.github.com>
2025-06-17 11:32:07 +0900

    Update importantFile.py

--- importantFile.py
+++ importantFile.py
@@ -8,6 +8,67 @@ from tensorflow.keras import layers
 from tesorflow.keras.models import Sequential
 import pathlib
 
+import matplotlib.pyplot as plt
+
 dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
 data_dir = tf.keras.utils.get_file('flower_photos.tar', origin = dataset_url, extract = True)
-data_dir = pathlib.Path
+data_dir = pathlib.Path(data_dir).with_suffix('')
+data_dir = data_dir / 'flower_photos'
+
+batch_size = 32
+img_height = 180
+img_width = 180
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split = 0.2,
+  subset = "training",
+  seed = 123,
+  image_size = (image_height, img_width),
+  batch_size = batch_size)
+)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+class_names = train_ds.class_names
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size = AUTOTUNE)
+vals_ds = val_ds.cache().prefectch(buffer_size = AUTOTUNE)
+
+normalization_layer = layers.Rescaling(1./255)
+normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+image_batch, labels_patch = next(iter(normalized_ds))
+
+num_classes = len(class_names)
+
+model = Sequential([
+  layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  layers.Conv2D(16, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+epochs=10
+history = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=epochs
+)
